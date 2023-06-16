89a8e3fa4ed396392695d20a817e163dcd06de36
yangx42 <49226140+yangx42@users.noreply.github.com>
2023-06-16 10:44:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,10 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-query = "SELECT * FROM fruit_load_list;"
+#query = "SELECT * FROM fruit_load_list;"
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute(query)
+my_cur.execute("SELECT * FROM fruit_load_list;")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
